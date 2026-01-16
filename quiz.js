f6460f7f0c38950ed8ecba41e597321785f9ef24
Evgeniy-makdak <makdak23@mail.ru>
2026-01-16 13:55:13 +0300

    Перенёс создание параграфов (вопросов) в массив файла js из html

--- quiz.js
+++ quiz.js
@@ -1,12 +1,22 @@
+const [question_1, question_2, question_3] = document.querySelectorAll('#test p')
 const [input_1, input_2, input_3] = document.querySelectorAll('#test input');
 const [button_1, button_2, button_3] = document.querySelectorAll('#test button');
+
+const questionData = [
+    'Напишите название языка типизации для JavaScript',
+    'Начиная с какого стандарта ES в JS не рекомендуется использовать var для объявления переменной?',
+    'С помощью какой браузерной функции можно вывести сообщение на экран?'
+];
 
 const quizData = [
     'TypeScript',
     'ES6',
     'alert'
-]
+];
 
+const newParagraph_1 = document.createElement('p');
+newParagraph_1.textContent = questionData[0];
+question_1.parentNode.appendChild(newParagraph_1);
 
 button_1.addEventListener('click', function() {
     if (input_1.value === quizData[0]) {
@@ -35,6 +45,10 @@ button_1.addEventListener('click', function() {
     }
 })
 
+const newParagraph_2 = document.createElement('p');
+newParagraph_2.textContent = questionData[1];
+question_2.parentNode.appendChild(newParagraph_2);
+
 button_2.addEventListener('click', function() {
     if (input_2.value === quizData[1]) {
         const newSpan = document.createElement('span');
@@ -61,6 +75,10 @@ button_2.addEventListener('click', function() {
         }, 5000)
     }
 })
+
+const newParagraph_3 = document.createElement('p');
+newParagraph_3.textContent = questionData[2];
+question_3.parentNode.appendChild(newParagraph_3);
 
 button_3.addEventListener('click', function() {
     if (input_3.value === quizData[2]) {
